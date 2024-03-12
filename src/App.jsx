7a9e72043a2144a4160b1d32ddd67af2cd1ddf59
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-12 13:32:05 -0400

    Refactor router to include Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Products from "./pages/Products/Products";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "products/:category", element: <Products /> },
+    ],
   },
 ]);
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
