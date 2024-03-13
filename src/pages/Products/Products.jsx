123ccf1dadb2dd4b33572059781fa82d3e10ac77
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-13 12:14:01 -0400

    Add product cards to the page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,15 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetchCategory from "../../hooks/useFetchCategory";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import styles from "./Products.module.css";
 
 const Products = () => {
   const { category } = useParams();
+  const { products, error, loading } = useFetchCategory(category);
 
-  // Get products in category
+  const Page = (content) => {
+    return (
+      <div className={styles.productsPage}>
+        <h1 className={styles.title}>
+          {category === "jewelery" ? "jewelry" : category}
+        </h1>
+        <div className={styles.container}>{content}</div>
+      </div>
+    );
+  };
 
-  return (
-    <>
-      <h1>{category}</h1>
-    </>
-  );
+  if (error) return Page(<p>Error</p>);
+  if (loading) return Page(<p>Loading...</p>);
+  return Page(products.map((p) => <ProductCard key={p.id} product={p} />));
 };
 
 export default Products;
