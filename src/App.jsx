5ce1ecce89b3d98fb038aad470c3463c159c18ba
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-14 09:33:06 -0400

    Implement cart tracking using context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
+import { createContext, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const Layout = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "products/:category", element: <Products /> },
-    ],
-  },
-]);
+export const CartContext = createContext({
+  cart: [],
+  addToCart: () => {},
+});
 
 const App = () => {
+  // Cart management
+  const [cart, setCart] = useState({});
+
+  const addToCart = (product, quantity) => {
+    const id = product.id;
+    console.log(product);
+    if (id in cart) {
+      const oldQuantity = cart[id].quantity;
+      const newCart = {
+        ...cart,
+        [id]: { ...cart[id], quantity: oldQuantity + quantity },
+      };
+      setCart(newCart);
+    } else {
+      const newCart = { ...cart, [id]: { quantity: quantity, ...product } };
+      setCart(newCart);
+    }
+  };
+
+  // App Layout and Router
+  const Layout = () => {
+    return (
+      <CartContext.Provider value={{ cart, addToCart }}>
+        <Header />
+        <Outlet />
+      </CartContext.Provider>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "products/:category", element: <Products /> },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
