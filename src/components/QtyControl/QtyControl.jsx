b308491ff77a1a90eb8ddf66aa9c6c3a58c09b99
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-03-18 12:32:17 -0400

    Move input parser here and handle empty input

--- src/components/QtyControl/QtyControl.jsx
+++ src/components/QtyControl/QtyControl.jsx
@@ -1,6 +1,24 @@
 import styles from "./QtyControl.module.css";
 
 const QtyControl = ({ quantity, setQuantity }) => {
+  // Process and validate the quantity input
+  const parseInput = (input) => {
+    if (input === "") return null;
+    const inputInt = parseInt(input);
+    if (inputInt < 1) {
+      return 1;
+    }
+    return inputInt;
+  };
+
+  // Make sure quantity field isn't empty on focus out
+  const onFocusOut = (e) => {
+    if (e.target.value === "") {
+      e.target.value = 1;
+      setQuantity(parseInput(e.target.value));
+    }
+  };
+
   return (
     <div className={styles.container}>
       <button
@@ -13,6 +31,7 @@ const QtyControl = ({ quantity, setQuantity }) => {
       <input
         type="number"
         onChange={(e) => setQuantity(parseInput(e.target.value))}
+        onBlur={(e) => onFocusOut(e)}
         value={quantity || ""}
         aria-label="Quantity"
       />
